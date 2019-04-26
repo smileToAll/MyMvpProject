e0f95b2b756efd738dba4709aec0995d0c008d15
李洁 <lijie@inmyshow.com>
2019-04-26 11:31:10 +0800

    优化

--- app/src/main/java/com/example/administrator/myhttptest/amvp/activity/main/MainActivity.java
+++ app/src/main/java/com/example/administrator/myhttptest/amvp/activity/main/MainActivity.java
@@ -1,5 +1,17 @@
 package com.example.administrator.myhttptest.amvp.activity.main;
 
+import com.example.administrator.myhttptest.R;
+import com.example.administrator.myhttptest.amvp.activity.guide.GuideActivity;
+import com.example.administrator.myhttptest.amvp.fragment.main.Multis.MultiFragment;
+import com.example.administrator.myhttptest.amvp.fragment.main.meizhi.MeizhiFragment;
+import com.example.administrator.myhttptest.base.BaseActivity;
+import com.example.administrator.myhttptest.constant.AppConstant;
+import com.example.administrator.myhttptest.databinding.ActivityMainBinding;
+import com.example.administrator.myhttptest.widget.utils.IntentUtils;
+import com.example.administrator.myhttptest.widget.utils.SpUtil;
+import com.example.administrator.myhttptest.widget.utils.ToastUtils;
+import com.tbruyelle.rxpermissions2.RxPermissions;
+
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
@@ -11,18 +23,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
-import com.example.administrator.myhttptest.R;
-import com.example.administrator.myhttptest.amvp.activity.guide.GuideActivity;
-import com.example.administrator.myhttptest.amvp.fragment.main.meizhi.MeizhiFragment;
-import com.example.administrator.myhttptest.amvp.fragment.main.Multis.MultiFragment;
-import com.example.administrator.myhttptest.base.BaseActivity;
-import com.example.administrator.myhttptest.constant.AppConstant;
-import com.example.administrator.myhttptest.databinding.ActivityMainBinding;
-import com.example.administrator.myhttptest.widget.utils.IntentUtils;
-import com.example.administrator.myhttptest.widget.utils.SpUtil;
-import com.example.administrator.myhttptest.widget.utils.ToastUtils;
-import com.tbruyelle.rxpermissions2.RxPermissions;
-
 import io.reactivex.annotations.NonNull;
 import io.reactivex.functions.Consumer;
 
@@ -33,16 +33,17 @@ public class MainActivity extends BaseActivity {
     MenuItem currentMenuItem;
     Bundle savedInstanceState;
     long times;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.savedInstanceState=savedInstanceState;
+        this.savedInstanceState = savedInstanceState;
     }
 
     @Override
     public void setDataBinding() {
         super.setDataBinding();
-        binding=DataBindingUtil.setContentView(this,R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
     }
 
@@ -52,13 +53,13 @@ public class MainActivity extends BaseActivity {
         binding.mainNav.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@android.support.annotation.NonNull MenuItem item) {
-                if(currentMenuItem!=null&&currentMenuItem!=item) {
+                if (currentMenuItem != null && currentMenuItem != item) {
                     currentMenuItem = item;
                     currentMenuItem.setChecked(true);
-                    SpUtil.saveOrUpdateInt(AppConstant.CURRENT_MENU,currentMenuItem.getItemId());
+                    SpUtil.saveOrUpdateInt(AppConstant.CURRENT_MENU, currentMenuItem.getItemId());
                     switchFragment(getFragmentById(currentMenuItem.getItemId()));
                 }
-                binding.drawlayout.closeDrawer(Gravity.LEFT,true);
+                binding.drawlayout.closeDrawer(Gravity.LEFT, true);
                 return true;
             }
         });
@@ -71,15 +72,15 @@ public class MainActivity extends BaseActivity {
         binding.mainNav.setItemIconTintList(null);//为了使侧边栏里的图标显示原有的颜色
         binding.title.setText("首页");
         int anInt = SpUtil.findInt(AppConstant.CURRENT_MENU);
-        if(anInt!=-1){
-            currentMenuItem=binding.mainNav.getMenu().findItem(anInt);
+        if (anInt != -1) {
+            currentMenuItem = binding.mainNav.getMenu().findItem(anInt);
         }
-        if(currentMenuItem==null){
-            currentMenuItem=binding.mainNav.getMenu().findItem(R.id.meizhi);
+        if (currentMenuItem == null) {
+            currentMenuItem = binding.mainNav.getMenu().findItem(R.id.meizhi);
         }
-        if(currentMenuItem!=null){
+        if (currentMenuItem != null) {
             currentMenuItem.setChecked(true);
-            Fragment fragment=getFragmentById(currentMenuItem.getItemId());
+            Fragment fragment = getFragmentById(currentMenuItem.getItemId());
             switchFragment(fragment);
         }
     }
@@ -87,29 +88,32 @@ public class MainActivity extends BaseActivity {
     @Override
     public void onClick(View v) {
         super.onClick(v);
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.toggle:
                 binding.drawlayout.openDrawer(Gravity.LEFT);
                 break;
         }
     }
 
-    private void switchFragment(Fragment fragment){
-        if(fragment!=null){
-            if(currentFragment==null||!fragment.getClass().getName().equals(currentFragment.getClass().getName())){
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,fragment).commit();
-                currentFragment=fragment;
+    private void switchFragment(Fragment fragment) {
+        if (fragment != null) {
+            if (currentFragment == null || !fragment.getClass()
+                    .getName()
+                    .equals(currentFragment.getClass().getName())) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
+                currentFragment = fragment;
             }
         }
     }
-    private Fragment getFragmentById(int id){
-        Fragment fragment=null;
-        switch (id){
+
+    private Fragment getFragmentById(int id) {
+        Fragment fragment = null;
+        switch (id) {
             case R.id.meizhi:
-                fragment=new MeizhiFragment();
+                fragment = new MeizhiFragment();
                 break;
             case R.id.news:
-                fragment=new MultiFragment();
+                fragment = new MultiFragment();
                 break;
         }
         return fragment;
@@ -117,11 +121,11 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void onBackPressed() {
-        if(System.currentTimeMillis()-times<2000){
+        if (System.currentTimeMillis() - times < 2000) {
             finish();
-        }else{
-            times=System.currentTimeMillis();
-            ToastUtils.showShortToast(this,"再点一次，退出App");
+        } else {
+            times = System.currentTimeMillis();
+            ToastUtils.showShortToast(this, "再点一次，退出App");
         }
     }
 
@@ -130,10 +134,10 @@ public class MainActivity extends BaseActivity {
         rxPermissions.request(permissions).subscribe(new Consumer<Boolean>() {
             @Override
             public void accept(@NonNull Boolean granted) throws Exception {
-                if (granted){
+                if (granted) {
                     Toast.makeText(MainActivity.this, "已获取权限", Toast.LENGTH_SHORT).show();
                     IntentUtils.goActivity(new Intent(MainActivity.this, GuideActivity.class));
-                }else {
+                } else {
                     Toast.makeText(MainActivity.this, "已拒绝一个或以上权限", Toast.LENGTH_SHORT).show();
                 }
             }
